7e1a4669da24f8331da3864d76f0600f5ee94dd8
Karthik-dev-js <189111789+Karthik-dev-js@users.noreply.github.com>
2024-12-11 12:46:34 +0530

    Add feature listing items
    * added a feature listing item in packingList component from a
    temporary array using map method with a help of another component
    called Item and improved styling

--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Item from "./Item";
 
-const PackingList = () => {
-  return <div className="list">List</div>;
+const PackingList = ({ initialItems }) => {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default PackingList;
