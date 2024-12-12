51838a1321797f331d1f31e04e3777c6d8ce9ba8
Karthik-dev-js <189111789+Karthik-dev-js@users.noreply.github.com>
2024-12-12 20:30:10 +0530

    fix bug on the initial displaying list

--- src/App.js
+++ src/App.js
@@ -4,13 +4,8 @@ import Logo from "./components/Logo";
 import PackingList from "./components/PackingList";
 import Stats from "./components/Stats";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-];
-
 function App() {
-  const [items, setItems] = useState(initialItems);
+  const [items, setItems] = useState([]);
 
   function handleAddItems(item) {
     setItems((items) => [...items, item]);
